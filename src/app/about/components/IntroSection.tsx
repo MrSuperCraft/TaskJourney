9b9326a5b329f9bd353b2e665aa24cd22b360018
mrsupercraft <itamarha2006@gmail.com>
2024-06-25 01:20:57 +0300

    Update: fix for no opacity sections

--- src/app/about/components/IntroSection.tsx
+++ src/app/about/components/IntroSection.tsx
@@ -5,7 +5,7 @@ import { Image } from "@nextui-org/image";
 
 const IntroductionSection = () => {
     return (
-        <div className="transition-opacity duration-500 ease-in-out opacity-0 animate-fadeIn">
+        <div className=" duration-500 ease-in-out">
             <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-6">
                 <h1 className="text-4xl font-bold mb-2 font-inter text-primary-brand-700 dark:text-primary-brand-300">The Genesis of TaskJourney</h1>
                 <p className="text-lg sm:text-base md:text-lg lg:text-lg xl:text-xl font-lato text-gray-600 dark:text-gray-300">
--- src/app/about/components/MissionGoalsSection.tsx
+++ src/app/about/components/MissionGoalsSection.tsx
@@ -3,7 +3,7 @@ import { Image } from '@nextui-org/react';
 
 const MissionGoalsSection = () => {
     return (
-        <div className="transition-opacity duration-500 ease-in-out opacity-0 animate-fadeIn">
+        <div className=" duration-500 ease-in-out">
             <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-6">
                 <h2 className="text-2xl font-bold mb-2 font-inter text-primary-brand-700 dark:text-primary-brand-300">My Mission and Goals</h2>
                 <p className="text-lg sm:text-base md:text-lg lg:text-lg xl:text-xl font-lato text-gray-600 dark:text-gray-300">
