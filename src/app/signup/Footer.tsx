92e5134dffd60f27f2ce5c4ba6bcb14b9cfe8a69
mrsupercraft <itamarha2006@gmail.com>
2024-06-30 22:59:28 +0300

    Update: added policy compliance disclaimer

--- src/app/signup/Footer.tsx
+++ src/app/signup/Footer.tsx
@@ -11,6 +11,11 @@ const Footer = () => (
                 </span>
             </Link>
         </span>
+        <div className="mt-4 text-center">
+            <span className="text-gray-600 dark:text-gray-300">
+                By using this website, you are accepting our <Link href="/terms-of-service"><span className="text-teal dark:text-primary-brand-600 hover:underline ml-2">Terms of Service</span></Link> and <Link href="/privacy-policy"><span className="text-teal dark:text-primary-brand-600 hover:underline ml-2">Privacy Policy</span></Link>.
+            </span>
+        </div>
     </div>
 );
 
