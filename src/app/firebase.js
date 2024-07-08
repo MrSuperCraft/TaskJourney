bd1064c7bcdec9bd69a3f791daf2d4d35bd51eac
mrsupercraft <itamarha2006@gmail.com>
2024-07-08 22:08:39 +0300

    update: fixed config

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -2,19 +2,9 @@
 import { initializeApp } from "firebase/app";
 import 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import { doc, getFirestore } from "firebase/firestore";
-import { getStorage } from '@firebase/storage'
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from '@firebase/storage';
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FBASE_API_KEY,
@@ -22,7 +12,7 @@ const firebaseConfig = {
     projectId: process.env.NEXT_PUBLIC_FBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FBASE_STORAGEBUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
+    appId: process.env.NEXT_PUBLIC_FBASE_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
@@ -33,9 +23,6 @@ const auth = getAuth(app);
 const db = getFirestore();
 const storage = getStorage(app);
 
-export const verifyIdToken = (token) => {
-    return auth.verifyIdToken(token);
-};
 
 
 const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_KEY);
@@ -43,8 +30,6 @@ const model = genAI.getGenerativeModel({ model: 'gemini-1.5flash' });
 
 async function generateAI(userPrompt) {
     try {
-
-
         const prompt = userPrompt;
         const result = await model.generateContent(prompt);
         const response = result.response;
@@ -56,10 +41,4 @@ async function generateAI(userPrompt) {
     }
 }
 
-
-
-
 export { app, auth, db, storage, generateAI };  // Export app and auth for use in other modules
-
-
-
